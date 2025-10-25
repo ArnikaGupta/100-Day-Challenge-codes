53197a11a0a5fb114751d0e3bb3bb9a3585a2bfe
ArnikaGupta <arnikaguptaaa@gmail.com>
2025-10-25 23:48:20 +0530

    Add files via upload

---
+++ q66.c
@@ -0,0 +1,49 @@
+//Q66: Insert an element in a sorted array at the appropriate position.
+
+/*
+Sample Test Cases:
+Input 1:
+5
+1 2 4 5 6
+3
+Output 1:
+1 2 3 4 5 6
+
+*/
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, element, pos;
+
+    printf("Enter number of elements in the sorted array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    for (pos = 0; pos < n; pos++) {
+        if (element < arr[pos]) {
+            break;
+        }
+    }
+
+    for (i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos] = element;
+    n++; 
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
